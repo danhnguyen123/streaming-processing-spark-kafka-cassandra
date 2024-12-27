e4fc1efb370a6e3014047a7f25b722c898bd49bd
Your Name <you@example.com>
2024-12-27 22:55:11 +0700

    Update

--- spark_stream.py
+++ spark_stream.py
@@ -127,7 +127,8 @@ def create_selection_df_from_kafka(spark_df):
     ])
 
     sel = spark_df.selectExpr("CAST(value AS STRING)") \
-        .select(from_json(col('value'), schema).alias('data')).select("data.*")
+                .select(from_json(col("value").cast("string"), schema).alias("data")) \
+                .select("data.*")
     print(sel)
 
     return sel
