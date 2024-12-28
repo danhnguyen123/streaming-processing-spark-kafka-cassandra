edda2b5e27ad0a85857faa1ae1e609464a410f70
Your Name <you@example.com>
2024-12-28 18:04:27 +0700

    Update

--- spark_stream.py
+++ spark_stream.py
@@ -81,7 +81,7 @@ def create_spark_connection():
     try:
         s_conn = SparkSession.builder \
             .appName('SparkDataStreaming') \
-            .config('spark.jars.packages', "org.apache.spark:spark-sql-kafka-0-10_2.13:3.4.0,com.datastax.spark:spark-cassandra-connector_2.13:3.4.0") \
+            .config('spark.jars.packages', "org.apache.spark:spark-sql-kafka-0-10_2.13:3.4.0,com.datastax.spark:spark-cassandra-connector_2.12:3.4.0") \
             .config('spark.cassandra.connection.host', 'cassandra') \
             .getOrCreate()
 
