8c1424d78d1ba18c99a73b000dfadd38109e4928
Your Name <you@example.com>
2024-12-28 18:38:09 +0700

    Update

--- command.sh
+++ command.sh
@@ -1,11 +1,12 @@
 # Install python package
 pip3 install -r requirements.txt
 
-# Produce message to Kafka
+# Produce message to Kafka: Open localhost:9021 to see Control Center Kafka
 python3 produce_message.py
 
-# Spark Submit
+# Spark Submit: Open localhost:4040 to see Spark app
 docker exec spark-master bash -c "python3 /opt/workspace/spark_stream.py"
+
 
 # Query cassandra
 docker exec -it cassandra cqlsh -u cassandra -p cassandra localhost 9042
